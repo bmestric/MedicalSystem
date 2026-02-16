b9ac169414e182100b7a6dc894e14b38076160cd
boris <bmestric@algebra.hr>
2026-02-16 14:17:26 +0100

    creating custom attributes

---
+++ ORM/Attributes/ColumnAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnAttribute : Attribute
+    {
+        public string? Name { get; set; }
+        public string? TypeName { get; set; }
+        public int Length { get; set; }
+
+        public ColumnAttribute()
+        {
+
+        }
+
+        public ColumnAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ ORM/Attributes/ForeignKeyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ForeignKeyAttribute : Attribute
+    {
+        public string ReferencedTable { get; }
+        public string ReferencedColumn { get; set; } = "id";
+
+        public bool Nullable { get; set; } = false;
+        public string? OnDelete { get; set; } = "NO ACTION";
+        public string? OnUpdate { get; set; } = "NO ACTION";
+
+        public ForeignKeyAttribute(string referencedTable)
+        {
+            ReferencedTable = referencedTable;
+        }
+    }
+}
