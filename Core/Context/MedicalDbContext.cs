f3748d8dbc1467ddb7aaea1dc1ac81fb5d475266
boris <bmestric@algebra.hr>
2026-03-02 14:21:30 +0100

    Context

---
+++ Core/Context/MedicalDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using ORM.Querying;
+
+namespace Core.Context
+{
+    public class MedicalDbContext : DatabaseContext
+    {
+        public DbSet<Patient> Patients => Set<Patient>();
+        public DbSet<Doctor> Doctors => Set<Doctor>();
+        public DbSet<Disease> Diseases => Set<Disease>();
+        public DbSet<MedicalHistory> MedicalHistories => Set<MedicalHistory>();
+        public DbSet<Medication> Medications => Set<Medication>();
+        public DbSet<Prescription> Prescriptions => Set<Prescription>();
+        public DbSet<Appointment> Appointments => Set<Appointment>();
+
+        public MedicalDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        protected override void OnConfiguring()
+        {
+        }
+    }
+}
